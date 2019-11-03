afa1f6dec6dd3e2e6db8f66efbc741055d81a3c2
Chris <chris.peterson444@gmail.com>
2019-11-03 14:34:44 -0800

    Initial commit with headers for functions

--- neural_network.py
+++ neural_network.py
@@ -1,2 +1,35 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+# Helper function to predict an output (0 or 1)
+# Model is the current version of the model {'W1':W1, 'b1’:b1, 'W2':W2, 'b2',b2}. It's a dictionary
+# x is one sample (without the label)
+def predict(model, x):
+	pass
+
+# Helper function to evaluate the total loss on the dataset
+# model is the current version of the model {'W1':W1, 'b1’:b1, 'W2':W2, 'b2',b2}. It's a dictionary
+# X is all the training data
+# y is the training labels
+def calculate_loss(model, X, y):
+	pass
+
+# This function learns parameters for the neural network and returns the model.
+# - X is the training data
+# - y is the training labels
+#  nn_hdim: Number of nodes in the hidden layer
+# - num_passes: Number of passes through the training data for gradient descent
+#- print_loss: If True, print the loss every 100 iterations
+def build_model(X, y, nn_hdim, num_passes=20000, print_loss=False):
+	pass
+
+def plot_decision_boundary(pred_func, X, y):
+	# Set min and max values and give it some padding
+	x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
+	y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
+	h = 0.01
+	# Generate a gride of points with distance h between them
+	xx, yy = np.meshgrid(np.arrange(x_min, x_max, h), np.arrange(y_min, y_max, h))
+	# Predict the function value for the whole grid
+	Z = pred_func(np.c_[xx.ravel(), yy.ravel()])
+	Z = Z.reshape(xx.shape)
