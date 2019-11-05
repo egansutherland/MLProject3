18331c6397bfe040f0cbead111282fca2ec35ebf
Chris Peterson <Chris.peterson444@gmail.com>
2019-11-05 00:02:48 -0800

    Shape issues with nn_hdim = 1 due to numpy. Fixed. Still needs testing but need to get this on a faster machine...

--- neural_network.py
+++ neural_network.py
@@ -13,7 +13,7 @@ def predict(model, x):
 	z = np.add(np.matmul(h,model["W2"]), model["b2"])
 	z = np.exp(z)
 	z = np.true_divide(z,np.sum(z))
-	return z	
+	return np.argmax(z)	
 
 # Helper function to evaluate the total loss on the dataset
 # model is the current version of the model {'W1':W1, 'b1’:b1, 'W2':W2, 'b2',b2}. It's a dictionary
@@ -41,47 +41,125 @@ def build_model(X, y, nn_hdim, num_passes=20000, print_loss=False):
 	W1 = np.random.rand(len(X[0]), nn_hdim)
 	W2 = np.random.rand(nn_hdim, 2)
 	b1 = np.random.rand(nn_hdim)
-	b2 = np.random.rand(len(X[0]))
+	b2 = np.random.rand(2)
 	# Gradient variables for back-prop
-	grad_W1 = np.zeros((2,nn_hdim))
+	grad_W1 = np.zeros((len(X[0]),nn_hdim))
 	grad_W2 = np.zeros((nn_hdim, 2))
 	grad_b1 = np.zeros(nn_hdim)
-	grad_b2 = np.zeros(len(X[0]))
-
+	grad_b2 = np.zeros(2)
+	# Setup model variable
 	model = {"W1": W1, "W2": W2, "b1": b1, "b2": b2}
 
 	for i in range(0,num_passes):
 		for j in range(0, len(X)):
-			y_hat = predict(model, X[j])
+
+
+			# Forward propagation. Same as predict, but keeps y_hat as a 2d array
+			a = np.add(np.matmul(X[j],W1),b1)
+			h = np.tanh(a)
+			z = np.add(np.matmul(h,W2),b2)
+			z = np.exp(z)
+			y_hat = np.true_divide(z,np.sum(z))
+
 			# print the loss every 1000 epochs
 			if print_loss and i == 1000:
 				loss = calculate_loss(model, X, y)
 				print(loss)
+
+			# Some set up for the back propagation
+			
+			# Numpy representation vectors is troublesome for multiplication:
+			# We know:    b2 = [ b1 b2]
+			# and the transpose is:    [ b1
+			#                            b2 ]
+			# Where the original is a row vector and the other is a column vector
+			# This distinction in shape is important for matrix multiplication
+			# but since it's one dimensional the numpy view seeminlgy makes no
+			# distinction. It just returns an iterator. To fix this, you have
+			# to force it into a 2D array simply by enclosing the single array
+			# into another array. Like so: [ [b1, b2] ] 
+			# 
+			# Note this is an issue only when nn_hdim is 1
+			# so below is a check for that condition and applies needed fixes
+			if nn_hdim == 1:
+				h = np.array([h])
+				y_hat = np.array([y_hat])
+
+			# Since X is an array 1D feature vectors, the following conversion
+			# is always necessary
+			sample = np.array([X[j]])
+
+			#Back propagation	
 			dl_dy = y_hat - y[j]
-			a = np.matmul(X,W1) + b1
-			dl_da = np.multiply((1 - (np.square(np.tanh(a)))),np.matmul(dl_dy, np.transpose(W2)))
-			h = np.tanh(np.add(np.matmul(X,W1),b1))
+			dl_da = np.multiply(1 - np.square(h), np.matmul(dl_dy, np.transpose(W2)))
+			# print('h shape', h.shape)
+			# print('h', h)
+			# print('h transpose shape' ,np.transpose(h).shape)
+			# print(np.transpose(h))
+			# print('dl_dy shape', dl_dy.shape)
+			# print('dl_dy', dl_dy)
 			dl_w2 = np.matmul(np.transpose(h),dl_dy)
-			dl_w1 = np.matmul(np.transepose(X), dl_da) 
+			dl_w1 = np.matmul(np.transpose(sample), dl_da) 
 			dl_b1 = copy.deepcopy(dl_da)
 			dl_b2 = copy.deepcopy(dl_dy)
+
+			# print('--------------------------')
+			# print('dl_dy shape', dl_dy.shape)
+			# print('dl_da shape', dl_da.shape)
+			# print('dl_w2 shape', dl_w2.shape)
+			# print('grad_W2 shape', grad_W2.shape)
+			# print('dl_w1 shape', dl_w1.shape)
+			# print('grad_W1 shape', grad_W1.shape)
+			# print('dl_b1 shape', dl_b1.shape)
+			# print('grad_b1 shape', grad_b1.shape)
+			# print('dl_b2.shape', dl_b2.shape)
+			# print('grad_b2 shape', grad_b2.shape)
+			# print('--------------------------')
 
 			grad_W1 = grad_W1 + dl_w1
 			grad_W2 = grad_W2 + dl_w2
 			grad_b1 = grad_b1 + dl_b1
 			grad_b2 = grad_b2 + dl_b2
 
-		# finished one epoch, update weights and biases
-		W1 = np.subtract(W1, np.multiply(grad_W1, learning_rate))
-		W2 = np.subtract(W2, np.multiply(grad_W2, learning_rate))
-		b1 = np.subtract(b1, np.multiply(grad_b1, learning_rate))
-		b2 = np.subtract(b2, np.multiply(grad_b2, learning_rate))
+		## finished one epoch
+
+		# Fix shape change
+		grad_b1 = np.reshape(grad_b1, b1.shape)
+		grad_b2 = np.reshape(grad_b2, b2.shape)
+		# print('grad_W1', grad_W1)
+		# print('shape grad_W1', grad_W1.shape)
+		# print('shape W1', W1.shape)
+		# print('grad_W2', grad_W2)
+		# print('shape grad_W2', grad_W2.shape)
+		# print('shape W2', W2.shape)
+		# print('grad_b1', grad_b1)
+		# print('shape grad_b1', grad_b1.shape)
+		# print('shape b1', b1.shape)
+		# print('grad_b2', grad_b2)
+		# print('shape grad_b2', grad_b2.shape)
+		# print('shape b2', b2.shape)
+
+		# Get average gradients
+		grad_W1 = grad_W1/len(X)
+		grad_W2 = grad_W2/len(X)
+		grad_b1 = grad_b1/len(X)
+		grad_b2 = grad_b2/len(X)
+
+		# update weights and biases
+		W1 = np.subtract(W1, grad_W1 * learning_rate)
+		W2 = np.subtract(W2, grad_W2 * learning_rate)
+		b1 = np.subtract(b1, grad_b1 * learning_rate)
+		b2 = np.subtract(b2, grad_b2 * learning_rate)
 		model["W1"] = W1
 		model["W2"] = W2
 		model["b1"] = b1
 		model["b2"] = b2
 
-		#keep going
+		# Clear average gradient counters and keep going
+		grad_W1 = np.zeros((len(X[0]),nn_hdim))
+		grad_W2 = np.zeros((nn_hdim, 2))
+		grad_b1 = np.zeros(nn_hdim)
+		grad_b2 = np.zeros(2)
 
 	return model
 			
