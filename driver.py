7ed160556188f4c78eeb9f05b774227fa8b2c730
Chris Peterson <Chris.peterson444@gmail.com>
2019-11-07 19:39:20 -0800

    Decent SGD run

--- driver.py
+++ driver.py
@@ -2,7 +2,7 @@
 # @Author: Chris Peterson
 # @Date:   2019-11-03 18:39:25
 # @Last Modified by:   Chris Peterson
-# @Last Modified time: 2019-11-07 19:07:09
+# @Last Modified time: 2019-11-07 19:35:24
 from neural_network import *
 import numpy as np
 from sklearn.datasets import make_moons
@@ -34,8 +34,9 @@ hidden_layer_dimensions = [1, 2, 3, 4]
 for i, nn_hdim in enumerate(hidden_layer_dimensions):
     plt.subplot(5, 2, i + 1)
     plt.title('HiddenLayerSize%d' % nn_hdim)
-    model = build_model(X, y, nn_hdim, print_loss=True)
+    model = build_model(X, y, nn_hdim, print_loss=False, num_passes=400000)
     print('model', model)
     plot_decision_boundary(lambda X: np.array([predict(model, x) for x in X]), X, y)
     # plot_decision_boundary(lambda x:  predict(model,x),X,y)
-plt.show()
+# plt.show()
+plt.savefig('foo.png')
